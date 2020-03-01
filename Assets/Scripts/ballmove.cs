7d670a650aebabff8aed15fd51ce48465192c0d4
jinyouli <229338934@qq.com>
2020-03-01 16:37:09 +0800

    完成所有关卡，还有部分bug要改
    完成所有关卡，还有部分bug要改

--- Assets/Scripts/ballmove.cs
+++ Assets/Scripts/ballmove.cs
@@ -7,7 +7,7 @@ public class ballmove : MonoBehaviour {
 	public float speed = 6;
 	public float start_point = 2.0f;
 	public float end_point = -3.48f;
-	void Update()
+	void FixedUpdate()
 	{
 		transform.position += Vector3.up * speed * Time.deltaTime;
 		if (transform.position.y>=start_point || transform.position.y <= end_point)
